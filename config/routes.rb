7fbfb4d8345f0afe41894f670be0e2a4d14a893a
zahid208 <zahidhanif208@gmail.com>
2018-02-07 02:20:53 -0800

    first on production commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
 
 
   resources :users
-
+  resources :account_activations, only: [:edit]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name:  "Example User",
     email: "example@railstutorial.org",
     password:              "foobar",
     password_confirmation: "foobar",
-    admin: true)
-
+    admin:     true,
+    activated: true,
+    activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -19,5 +13,7 @@ User.create!(name:  "Example User",
   User.create!(name:  name,
       email: email,
       password:              password,
-      password_confirmation: password)
+      password_confirmation: password,
+      activated: true,
+      activated_at: Time.zone.now)
 end
